625e5bfe775ff07c4818c04efb301c22b1096dff
André Rodrigues <andre.cs.rodrigues@gmail.com>
2021-05-09 18:17:51 -0300

    started exercises repo

---
+++ arrays-filter.js
@@ -0,0 +1,112 @@
+// EX 01 -> Crie um novo array a partir do array 'lista',
+// que contenha somente os elementos que são maiores ou iguais a 5.
+const ex1 = () => {
+  const lista = [10, 0, 9, 1, 8, 2, 7, 3, 6, 4, 5];
+
+  const novaLista = []; // Substitua pelo seu código
+
+  console.log("ex1", novaLista);
+};
+ex1();
+
+// EX 02 -> Crie um novo array a partir do array 'lista',
+// que contenha somente os elementos que são números pares.
+const ex2 = () => {
+  const lista = [10, 0, 9, 1, 8, 2, 7, 3, 6, 4, 5];
+
+  const novaLista = []; // Substitua pelo seu código
+
+  console.log("ex2", novaLista);
+};
+ex2();
+
+// EX 03 -> Crie um novo array a partir do array 'lista',
+// que contenha somente os elementos que são números pares
+// e que são maiores que 5.
+const ex3 = () => {
+  const lista = [10, 0, 9, 1, 8, 2, 7, 3, 6, 4, 5];
+
+  const novaLista = []; // Substitua pelo seu código
+
+  console.log("ex3", novaLista);
+};
+ex3();
+
+// EX 04 -> Crie um novo array a partir do array 'lista',
+// que contenha somente os elementos que são números ímpares
+// ou que são iguais ou menores que 5.
+const ex4 = () => {
+  const lista = [10, 0, 9, 1, 8, 2, 7, 3, 6, 4, 5];
+
+  const novaLista = []; // Substitua pelo seu código
+
+  console.log("ex4", novaLista);
+};
+ex4();
+
+// EX 05 -> Crie um novo array a partir do array 'lista',
+// somente com os elementos que possuem a letra 'n'.
+const ex5 = () => {
+  const lista = ["Banana", "Laranja", "Maçã", "Manga"];
+
+  const novaLista = []; // Substitua pelo seu código
+
+  console.log("ex5", novaLista);
+};
+ex5();
+
+// EX 06 -> Crie um novo array a partir do array 'lista',
+// somente com os elementos que possuem mais de 5 letras.
+const ex6 = () => {
+  const lista = ["Banana", "Laranja", "Maçã", "Manga"];
+
+  const novaLista = []; // Substitua pelo seu código
+
+  console.log("ex6", novaLista);
+};
+ex6();
+
+// EX 07 -> Crie um novo array a partir do array 'lista',
+// somente com os elementos que possuem a letra 'n' mas que não seja
+// na terceira posição.
+const ex7 = () => {
+  const lista = ["Banana", "Laranja", "Maçã", "Manga"];
+
+  const novaLista = []; // Substitua pelo seu código
+
+  console.log("ex7", novaLista);
+};
+ex7();
+
+// EX 08 -> Crie um novo array a partir do array 'lista',
+// somente com os elementos que possuem a letra 'n' no nome e que
+// o atributo 'qtd' seja maior ou igual a 200.
+const ex8 = () => {
+  const lista = [
+    { nome: "Banana", qtd: 210, emPromo: false },
+    { nome: "Laranja", qtd: 50, emPromo: true },
+    { nome: "Maçã", qtd: 130, emPromo: false },
+    { nome: "Manga", qtd: 200, emPromo: true },
+  ];
+
+  const novaLista = []; // Substitua pelo seu código
+
+  console.log("ex8", novaLista);
+};
+ex8();
+
+// EX 09 -> Crie um novo array a partir do array 'lista',
+// somente com os elementos que estão em promoção.
+const ex9 = () => {
+    const lista = [
+      { nome: "Banana", qtd: 210, emPromo: false },
+      { nome: "Laranja", qtd: 50, emPromo: true },
+      { nome: "Maçã", qtd: 130, emPromo: false },
+      { nome: "Manga", qtd: 200, emPromo: true },
+    ];
+  
+    const novaLista = []; // Substitua pelo seu código
+  
+    console.log("ex9", novaLista);
+  };
+  ex9();
